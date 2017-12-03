1be0fe28166fdd989f550333ac69fac67bbb6e7c
Cid Avarice <cid.avarice@gmail.com>
2017-12-03 15:49:11 -0500

    Fixed file position and made minor changes

---
+++ Dominoes/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Player1
+{
+    public readonly string name;
+    public readonly string teamName;
+    private int score = 0;
+
+    // need to address tiles in hand of player
+    public Player1(string name, string teamName = "Team 1")
+    {
+        // call user input and return values? 
+    }
+    
+    public void AddScore(int s)
+    {
+        score += s;
+    }
+    public int GetScore()
+    {
+        return score;
+    }
+}
