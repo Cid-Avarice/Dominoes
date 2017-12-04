babf7c4054a1586c81bf469e076ce9c65ab66b45
Guario29 <gpere111@fiu.edu>
2017-12-03 19:05:53 -0500

    player tile

--- Dominoes/Player.cs
+++ Dominoes/Player.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Dominoes;
+using System;
+
 
 public class Player1
 {
+
     public readonly string name;
     public readonly string teamName;
     private int score = 0;
+    public Tile[] hand = new Tile[7];
 
     // need to address tiles in hand of player
     public Player1(string name, string teamName = "Team 1")
@@ -16,8 +20,15 @@ public class Player1
     {
         score += s;
     }
+
     public int GetScore()
     {
         return score;
     }
+
+    public void AssignHand(Tile[] given)
+    {
+        hand = given;
+    }
+
 }
